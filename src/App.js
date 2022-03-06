e0cdbf4baa46329b6278aa386780824f26a9dbba
Wojciech Kurzak <wojciechkurzak@sitero.net>
2022-03-06 20:48:35 +0100

    Add fetch to server

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import './App.scss'
+import { useState, useEffect } from 'react'
 
 function App() {
+    const [data, setData] = useState('')
+
+    useEffect(() => {
+        fetch('http://localhost:5000/data')
+            .then((res) => res.json())
+            .then((data) => {
+                setData(data)
+                console.log(data)
+            })
+    }, [])
+
     return <p>Hello world!</p>
 }
 
