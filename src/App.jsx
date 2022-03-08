03ad512f3b047bc4d4602ca71acbb09910791abd
Wojciech Kurzak <wojciechkurzak@sitero.net>
2022-03-08 18:49:36 +0100

    Add pagination

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ function App() {
             .then((res) => res.json())
             .then((data) => {
                 setData(data)
-                console.log(data)
             })
     }, [])
 
--- src/items/ItemRender.jsx
+++ src/items/ItemRender.jsx
@@ -1,15 +1,35 @@
-import {useContext} from 'react'
+import {useState, useContext} from 'react'
+import ReactPaginate from 'react-paginate'
 import UserContext from '../UserContext'
 import ItemTemplate from './ItemTemplate'
 
 const ItemRender = () => {
     const data = useContext(UserContext)
 
+    const [pageNumber, setPageNumber] = useState(0)
+    const [items, setItems] = useState(data.prices.values.filter(element => element[14] === '0' && element))
+
+    const itemsPerPage = 13
+    const pagesVisited = pageNumber * itemsPerPage
+    const pageCount = Math.ceil(items.length / itemsPerPage)
+
+    const displayItems = items
+            .slice(pagesVisited, pagesVisited + itemsPerPage)
+            .map(item => <ItemTemplate key={item[13]} info={item}/>)
+
+    const pageChange = ({selected}) => {
+        setPageNumber(selected)
+    }
+
     return (
         <div>
-            {data.prices.values.map(element =>
-                element[14] === '0' && <ItemTemplate key={element[13]} info={element}/>
-            )}
+            {displayItems}
+            <ReactPaginate
+                previousLabel={'Prevoius'}
+                nextLabel={'Next'}
+                pageCount={pageCount}
+                onPageChange={pageChange}
+            />
         </div>
     )
 }
